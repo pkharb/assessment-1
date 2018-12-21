69d972022b941a81013e7318ac1503071b207cac
pk <pradeepkharb@hotmail.com>
2018-12-21 15:19:40 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const  
+    express = require('express'),
+    app = express(),
+    path = require('path'),
+    logger = require('morgan'),
+    PORT = 3000,
+    ejs = require('ejs'),
+    ejsLayouts = require('express-ejs-layouts'),
+    axios = require('axios');
+
+
+// ejs configuration
+app.set('view engine', 'ejs');
+
+// middleware
+app.use(ejsLayouts);
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname + 'public')));
+// app.use(express.static(path.join(__dirname, 'public', 'views')))
+
+
+// routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+// app.get('/', (req, res) => {
+//     res.render('posts');
+// });
+app.get('/posts', (req,res) => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(({ data }) => {
+        res.render('posts', {data})
+    });
+    
+});
+
+// listen to port
+app.listen(PORT, err => {
+    console.log(err || `listening to port: ${PORT}`)
+})
